aa3a875760d5c27694a015ab845ed352cbd092c5
romanprem0123 <romanprem0123@gmail.com>
2023-08-03 15:12:12 +0530

    new

---
+++ js/main.js
@@ -0,0 +1,205 @@
+// condition
+
+// let a = 20;
+// let b = 30;
+// let c = 10;
+// let d = 40;
+
+
+// 2
+
+// let tamil=parseInt(prompt("enter tamil sub mark"));
+// let english=parseInt(prompt("enter english sub mark"));
+// let maths=parseInt(prompt("enter maths sub mark"));
+// let science=parseInt(prompt("enter science sub mark"));
+// let social_science=parseInt(prompt("enter social_science sub mark"));
+
+
+
+// total = (tamil+english+maths+science+social_science);
+// console.log(total)
+
+// if(total){
+	
+	// console.log("Total Marks : "+total);
+	
+	// console.log("Average : "+(total/5));
+	
+// }
+// else{
+	// console.log("enter value numbers only");
+// }
+
+//3
+
+	// let temp = parseInt(prompt("enter temperature value"));
+
+	// if (temp < 0) 
+	  // {
+		// console.log("Freezing weather");
+	  
+	  // } 
+	 // else if (temp >= 0 && temp <= 10) {
+	  
+	  // console.log("Very Cold weather");
+
+	  // }
+	 // else if (temp > 10 && temp <= 20) {
+
+		// console.log("Cold weather");
+
+	  // } 
+	 // else if (temp > 20 && temp <= 30) {
+
+		// console.log("Normal weather");
+
+	  // } 
+	 // else if (temp > 30 && temp <= 40) {
+
+		// console.log("Hot weather");
+
+	  // } 
+	 // else if (temp >= 40) {
+
+		// console.log("Very Hot weather");
+
+	  // } 
+	 // else {
+
+		// console.log("temperature value enter Number");
+
+	  // }
+
+
+
+//Qus 4
+
+// let num = parseInt(prompt("enter numbers"));
+
+// if(num%2==0){
+	
+	// console.log("even");
+// }
+// else{
+	
+	// console.log("odd");
+// }
+
+// Qus 5
+
+// let a = parseInt(prompt("enter numbers"));
+// let b = parseInt(prompt("enter numbers"));
+
+// c=a+b;
+
+  // if(a==b){
+	   
+	  // console.log(c**3);
+  // }
+  // else{
+	  
+	  // console.log(c);
+  // }
+  
+  // Qus 6
+  
+  // let a = parseInt(prompt("enter numbers"));
+  // let b = parseInt(prompt("enter numbers"));
+  
+  // if((a+b==50)||(a==50)||(b==50)){
+	  
+	  // console.log("True");
+	  
+  // }
+  // else{
+	  
+	  // console.log("false");
+  // }
+ 
+ // Qus 7
+
+	// let a = parseInt(prompt("enter numbers"));
+	
+	// if()
+		
+// Qus 7
+
+
+	// let num1 = parseInt(prompt("enter numbers"));
+	
+	// if(num1%3==0){
+		
+		// console.log(" multiple of 3 ");
+	// }
+	// else if(num1%7==0){
+		
+		// console.log(" multiple of 7 ");
+	// }
+	// else{
+		
+		// console.log("not multiple");
+	// }
+	
+// Qus 8
+
+	// let a = parseInt(prompt("enter numbers"));
+	// let b = parseInt(prompt("enter numbers"));
+	// let c = parseInt(prompt("enter numbers"));
+	
+	// if(a>b && a>c){
+		
+		// console.log("largest num : "+a);
+	// }
+	// else if(b>a && b>c){
+		
+		// console.log("largest num : "+b);
+	// }
+	// else{
+		
+		// console.log("largest num : "+c);
+	// }
+	
+	
+	// let a = parseInt(prompt("enter numbers"));
+	
+	// if (0<a){
+		
+		// console.log("positive");
+	// }
+	// else{
+		
+		// console.log("negative");
+	// }
+	
+	
+	// let a = parseInt(prompt("enter numbers"));
+	// let b = parseInt(prompt("enter numbers"));
+	
+	// if((23<=a)&&(21<=b)){
+		
+		// console.log("eligible");
+	// }
+	// else{
+		
+		// console.log("not eligible");
+	// }
+
+//Qus 9
+
+	// let a = parseInt(prompt("enter numbers"));
+	// let b = parseInt(prompt("enter numbers"));
+	
+	// let a = 20;
+	// let b = 40;
+	
+	// result_1=100-a;
+	// result_2=100-b;
+	
+	// if(result_1>result_2){
+		// console.log("b");
+	// }
+	// else{
+		// console.log("a");
+	// }
+	
+	
